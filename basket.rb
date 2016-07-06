cad8db56edcbe1083d408058876b7a371e6f20d9
Krzysztof Zbudniewek <krzysztof.zbudniewek@gmail.com>
2016-07-06 14:18:43 +0200

    Add reader for warehouse in Basket class

--- basket.rb
+++ basket.rb
@@ -2,7 +2,7 @@ require_relative './basket_item'
 require_relative './warehouse'
 
 class Basket
-  attr_reader :items
+  attr_reader :items, :warehouse
 
   def initialize(warehouse)
     @warehouse = warehouse
@@ -10,12 +10,12 @@ class Basket
   end
 
   def add(product)
-    if @warehouse.in_stock?(product)
+    if warehouse.in_stock?(product)
       item = find_or_create_item(product)
 
       item.increase_quantity
 
-      @warehouse.decrease_product_status(product)
+      warehouse.decrease_product_status(product)
     else
       puts "Sorry, product is not available at the moment :("
     end
@@ -25,7 +25,7 @@ class Basket
     item = find_item(product)
     item_index = find_item_index(product)
 
-    @warehouse.increase_product_status(product)
+    warehouse.increase_product_status(product)
 
     if item.quantity == 0
       @items.delete_at(item_index)
